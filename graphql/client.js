be83048fa1f4dc560f294f0fc331adb2f8be9510
Ramsay Lanier <ramsaylanier@gmail.com>
2020-05-12 18:34:21 -0400

    update http link

--- graphql/client.js
+++ graphql/client.js
@@ -3,7 +3,7 @@ import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import { onError } from 'apollo-link-error'
 import { ApolloLink, split } from 'apollo-link'
-import { WebSocketLink } from 'apollo-link-ws'
+// import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 import fetch from 'node-fetch'
 
@@ -18,26 +18,26 @@ const host = 'ec2-54-175-88-74.compute-1.amazonaws.com:4000/graphql'
 //     })
 //   : null
 
-const httpLink = new HttpLink({
+const link = new HttpLink({
   uri: `http://${host}`,
   credentials: 'same-origin',
   fetch,
 })
 
-const link = process.browser
-  ? split(
-      // split based on operation type
-      ({ query }) => {
-        const definition = getMainDefinition(query)
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.operation === 'subscription'
-        )
-      },
-      httpLink,
-      httpLink
-    )
-  : httpLink
+// const link = process.browser
+//   ? split(
+//       // split based on operation type
+//       ({ query }) => {
+//         const definition = getMainDefinition(query)
+//         return (
+//           definition.kind === 'OperationDefinition' &&
+//           definition.operation === 'subscription'
+//         )
+//       },
+//       httpLink,
+//       httpLink
+//     )
+//   : httpLink
 
 const client = new ApolloClient({
   link: ApolloLink.from([
