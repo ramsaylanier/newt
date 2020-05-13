aa7f55033c4f338a7c5b8d8f5b3487f8420e071f
Ramsay Lanier <ramsaylanier@gmail.com>
2020-05-12 20:00:04 -0400

    something

--- graphql/client.js
+++ graphql/client.js
@@ -7,7 +7,7 @@ import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 import fetch from 'node-fetch'
 
-const host = 'http://ec2-54-175-88-74.compute-1.amazonaws.com/graphql'
+const host = 'ec2-54-175-88-74.compute-1.amazonaws.com/graphql'
 
 const wsLink = process.browser
   ? new WebSocketLink({
