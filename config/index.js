e62a6f764d84eb57ed4f37d1c3f031fc6c11ef34
Ramsay Lanier <ramsaylanier@gmail.com>
2020-05-12 20:58:19 -0400

    update database location

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const databaseName = process.env.DATABASE_NAME || 'newt'
-const databaseHost = process.env.DATABASE_HOST || 'http://0.0.0.0:8529'
+const databaseHost = process.env.DATABASE_HOST || 'http://54.175.88.74:8529'
 
 module.exports = {
   database: {
