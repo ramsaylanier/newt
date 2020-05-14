802051fb81b019cb5f9b140cf34ae69462614bbd
Ramsay Lanier <ramsaylanier@gmail.com>
2020-05-13 23:36:15 -0400

    update config

--- config/index.js
+++ config/index.js
@@ -1,26 +1,19 @@
 let config
-const isProd = process.env.NODE_ENV === 'production'
 
-console.log('IS PROD', isProd)
-
-if (isProd) {
-  config = {
-    database: {
-      host: process.env.DATABASE_NAME,
-      name: process.env.DATABASE_HOST,
-    },
-    graphql: {
-      host: process.env.GRAPHQL_HOST,
-    },
-    pusher: {
-      key: process.env.PUSHER_KEY,
-      secret: process.env.PUSHER_SECRET,
-    },
-  }
-} else {
-  config = require('./dev.json')
+config = {
+  database: {
+    host: process.env.DATABASE_NAME,
+    name: process.env.DATABASE_HOST,
+  },
+  graphql: {
+    host: process.env.GRAPHQL_HOST,
+  },
+  pusher: {
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+  },
 }
 
-console.log(config)
+console.log('CONFIG', config)
 
 module.exports = config
