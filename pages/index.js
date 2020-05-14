1725c398cc2f5c37f84c47e499a2ee56405cbbf7
Ramsay Lanier <ramsaylanier@gmail.com>
2020-05-14 14:48:37 -0400

    update landing page

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,23 @@ const Home = () => {
       </Head>
 
       <Layout>
-        <Box
-          width="100%"
-          height="100%"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Text fontSize="3rem">
-            Create A Page or Select One From The Sidebar
-          </Text>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Box p="2" maxWidth="800px">
+            <Text as="h3"> Welcome To Newt: Notes Worth Taking</Text>
+            <Text as="h5">Newt lets you take connected notes.</Text>
+            <Text>
+              Start by creating a page! You can add page links by clicking the
+              🔗 icon in the toolbar, and selecting a page you'd like to link.
+              You can also drag pages from the left into the content block to
+              create a page link. You can view a graph of all the connected
+              notes by clicking the graph icon next to the Create Page button.
+            </Text>
+            <Text as="strong">
+              Please be aware: this is a free public sandbox running on
+              free-tier everything. Thing are likely broken. Also, be nice.
+              There is zero tolerance for any of your shit.
+            </Text>
+          </Box>
         </Box>
       </Layout>
     </div>
