511d8428f17db3b48b8a4c427d4c49716a9e1f53
Ramsay Lanier <rlanier@novetta.com>
2022-10-22 00:18:47 -0400

    update pusher
    Signed-off-by: Ramsay Lanier <rlanier@novetta.com>

--- utils/pusherPlugin.js
+++ utils/pusherPlugin.js
@@ -1,28 +1,39 @@
 // https://www.the-guild.dev/graphql/envelop/docs/plugins
 
+const pusherAppId = process.env.NEXT_PUBLIC_PUSHER_APP_ID
+const pusherKey = process.env.NEXT_PUBLIC_PUSHER_KEY
+const pusherSecret = process.env.NEXT_PRIVATE_PUSHER_SECRET
+import Pusher from 'pusher'
+
+export const pusher = new Pusher({
+  appId: pusherAppId,
+  key: pusherKey,
+  secret: pusherSecret,
+  cluster: 'us2',
+  useTLS: true,
+})
+
 const pusherMap = {
-  CreatePage: ({ pusher, result }) => {
-    pusher &&
-      pusher.trigger('subscription', 'pageAdded', {
-        message: result?.data?.createPage,
-      })
+  CreatePage: ({ result }) => {
+    pusher.trigger('subscription', 'pageAdded', {
+      message: result?.data?.createPage,
+    })
   },
-  DeletePage: ({ pusher, result }) => {
-    pusher &&
-      pusher.trigger('subscription', 'pageDeleted', {
-        message: result?.data.deletePage,
-      })
+  DeletePage: ({ result }) => {
+    pusher.trigger('subscription', 'pageDeleted', {
+      message: result?.data.deletePage,
+    })
   },
 }
 
 export default {
   onExecute({ args }) {
     const { operationName } = args
-    const { pusher } = args.contextValue
+
     return {
       onExecuteDone({ result }) {
         if (pusherMap[operationName]) {
-          pusherMap[operationName]({ pusher, result })
+          pusherMap[operationName]({ result })
         }
       },
     }
