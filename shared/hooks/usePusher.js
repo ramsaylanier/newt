53bcac493e5949e754dd203cf412001306474989
Ramsay Lanier <rlanier@novetta.com>
2022-10-23 21:00:56 -0400

    update pusher deps
    Signed-off-by: Ramsay Lanier <rlanier@novetta.com>

--- shared/hooks/usePusher.js
+++ shared/hooks/usePusher.js
@@ -3,14 +3,14 @@ import Pusher from 'pusher-js'
 import { useApolloClient } from '@apollo/client'
 
 const pusherKey = process.env.NEXT_PUBLIC_PUSHER_KEY
+const pusher = new Pusher(pusherKey, {
+  cluster: 'us2',
+})
 
 const usePusher = (subscriptions) => {
   const client = useApolloClient()
 
   React.useEffect(() => {
-    const pusher = new Pusher(pusherKey, {
-      cluster: 'us2',
-    })
     const channel =
       pusher.channel('subscription') || pusher.subscribe('subscription')
 
